8dc2f381b4286df3ce9a618958428e58cdfa20ab
Rickie M. Proctor II <rickie@rickieproctor.com>
2024-05-22 19:49:21 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -32,29 +32,259 @@ function oceanwp_child_enqueue_parent_style() {
 
 add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
-
-
-
-
-// Include the library.
-require get_template_directory() . '/plugin-update-checker/plugin-update-checker.php';
-
-// Set up the update checker.
-add_action('init', function () {
-    $theme_update_checker = Puc_v4_Factory::buildUpdateChecker(
-        'https://github.com/rickpro2/wordpress-child-theme',
-        __FILE__,
-        'wordpress-child-theme'
-    );
-
-    // Optional: Set the branch that contains the stable release.
-    $theme_update_checker->setBranch('main');
-
-    // Optional: If you have a private repository, provide access token.
-    // $theme_update_checker->setAuthentication('your-token-here');
-});
+/* Start Adding Functions Below this Line */
 
 
 
 
 
+/* Create Admin/Webmaster User Role */
+add_role(
+    'webmaster', //  System name of the role.
+    __( 'Admin'  ), // Display name of the role.
+    array( 
+        'assign_product_terms'  => true,
+        'assign_shop_coupon_terms'  => true,
+        'assign_shop_order_terms'  => true,
+		'copy_posts'  => true,
+		'create_posts'  => true,
+		'create_users'  => true,
+		'delete_aggregator-records'  => true,
+		'delete_others_aggregator-records'  => true,
+		'delete_others_pages'  => true,
+		'delete_others_posts'  => true,
+		'delete_others_products'  => true,
+		'delete_others_shop_coupons'  => true,
+		'delete_others_shop_orders'  => true,
+		'delete_others_tribe_events'  => true,
+		'delete_others_tribe_organizers'  => true,
+		'delete_others_tribe_venues'  => true,
+		'delete_pages'  => true,
+		'delete_posts'  => true,
+		'delete_private_aggregator-records'  => true,
+		'delete_private_pages'  => true,
+		'delete_private_posts'  => true,
+		'delete_private_products'  => true,
+		'delete_private_shop_coupons'  => true,
+		'delete_private_shop_orders'  => true,
+		'delete_private_tribe_events'  => true,
+		'delete_private_tribe_organizers'  => true,
+		'delete_private_tribe_venues'  => true,
+		'delete_product'  => true,
+		'delete_product_terms'  => true,
+		'delete_products'  => true,
+		'delete_published_aggregator-records'  => true,
+		'delete_published_pages'  => true,
+		'delete_published_posts'  => true,
+		'delete_published_products'  => true,
+		'delete_published_shop_coupons'  => true,
+		'delete_published_shop_orders'  => true,
+		'delete_published_tribe_events'  => true,
+		'delete_published_tribe_organizers'  => true,
+		'delete_published_tribe_venues'  => true,
+		'delete_shop_coupon'  => true,
+		'delete_shop_coupon_terms'  => true,
+		'delete_shop_coupons'  => true,
+		'delete_shop_order'  => true,
+		'delete_shop_order_terms'  => true,
+  		'delete_shop_orders'  => true,
+		'delete_tribe_events'  => true,
+		'delete_tribe_organizers'  => true,
+		'delete_tribe_venues'  => true,
+		'delete_users'  => true,
+		'delete_wpforms_forms'  => true,
+		'edit_aggregator-records'  => true,
+		'edit_dashboard'  => true,
+		'edit_others_aggregator-records'  => true,
+		'edit_others_pages'  => true,
+		'edit_others_posts'  => true,
+		'edit_others_products'  => true,
+		'edit_others_shop_coupons'  => true,
+		'edit_others_shop_orders'  => true,
+		'edit_others_tribe_events'  => true,
+		'edit_others_tribe_organizers'  => true,
+		'edit_others_tribe_venues'  => true,
+ 		'edit_others_wpforms_forms'  => true,
+		'edit_pages'  => true,
+		'edit_posts'  => true,
+		'edit_private_aggregator-records'  => true,
+		'edit_private_pages'  => true,
+		'edit_private_posts'  => true,
+		'edit_private_products'  => true,
+		'edit_private_shop_coupons'  => true,
+		'edit_private_shop_orders'  => true,
+		'edit_private_tribe_events'  => true,
+		'edit_private_tribe_organizers'  => true,
+		'edit_private_tribe_venues'  => true,
+		'edit_product'  => true,
+		'edit_product_terms'  => true,
+ 		'edit_products'  => true,
+		'edit_published_aggregator-records'  => true,
+		'edit_published_pages'  => true,
+		'edit_published_posts'  => true,
+		'edit_published_products'  => true,
+		'edit_published_shop_coupons'  => true,
+		'edit_published_shop_orders'  => true,
+		'edit_published_tribe_events'  => true,
+		'edit_published_tribe_organizers'  => true,
+		'edit_published_tribe_venues'  => true,
+		'edit_shop_coupon'  => true,
+		'edit_shop_coupon_terms'  => true,
+		'edit_shop_coupons'  => true,
+		'edit_shop_order'  => true,
+		'edit_shop_order_terms'  => true,
+		'edit_shop_orders'  => true,
+		'edit_theme_options'  => false,
+		'edit_tribe_events'  => true,
+		'edit_tribe_organizers'  => true,
+		'edit_tribe_venues'  => true,
+		'edit_users'  => true,
+		'edit_wpforms_forms'  => true,
+		'editor'  => true,
+		'export'  => false,
+		'import'  => false,
+		'list_users'  => true,
+		'manage_categories'  => true,
+		'manage_links'  => true,
+		'manage_options'  => false,
+		'manage_product_terms'  => true,
+		'manage_shop_coupon_terms'  => true,
+		'manage_shop_order_terms'  => true,
+		'manage_woocommerce'  => true,
+		'moderate_comments'  => true,
+		'promote_users'  => true,
+  		'publish_aggregator-records'  => true,
+		'publish_pages'  => true,		
+		'publish_posts'  => true,
+		'publish_products'  => true,
+		'publish_shop_coupons'  => true,
+		'publish_shop_orders'  => true,
+		'publish_tribe_events'  => true,
+		'publish_tribe_organizers'  => true,
+		'publish_tribe_venues'  => true,
+		'publish_wpforms_forms'  => true,
+		'read'  => true,
+		'read_private_aggregator-records'  => true,
+		'read_private_pages'  => true,
+		'read_private_posts'  => true,
+		'read_private_products'  => true,
+		'read_private_shop_coupons'  => true,
+		'read_private_shop_orders'  => true,
+		'read_private_tribe_events'  => true,
+		'read_private_tribe_organizers'  => true,
+		'read_private_tribe_venues'  => true,
+		'read_private_wpforms_forms'  => true,
+		'read_product'  => true,
+		'read_shop_coupon'  => true,
+		'read_shop_order'  => true,
+		'remove_users'  => true,
+		'resume_plugins'  => false,
+		'resume_themes'  => false,
+		'tablepress_access_about_screen'  => true,
+		'tablepress_add_tables'  => true,
+		'tablepress_edit_tables'  => true,
+		'tablepress_export_tables'  => true,
+		'tablepress_import_tables'  => true,
+		'tablepress_list_tables'  => true,
+		'unfiltered_html'  => true,
+		'unfiltered_upload'  => true,
+		'upload_files'  => true,
+		'ure_admin_menu_access'  => true,
+		'ure_edit_posts_access'  => true,
+		'ure_export_roles'  => true,
+		'ure_front_end_menu_access'  => true,
+		'ure_import_roles'  => true,
+		'ure_meta_boxes_access'  => true,
+		'ure_nav_menus_access'  => true,
+		'ure_other_roles_access'  => true,
+		'ure_plugins_activation_access'  => true,
+		'ure_view_posts_access'  => true,
+		'ure_widgets_access'  => true,
+		'ure_widgets_show_access'  => true,
+		'view_site_health_checks'  => false,          
+    )
+);
+
+
+
+
+
+/* Remove Staff Member User Role */
+//remove_role("webmaster");
+//remove_role("admin");
+//remove_role("test");
+
+
+
+
+
+/* Display Page IDs */
+add_filter( 'manage_pages_columns', 'revealid_add_pages_id_column', 5 );
+add_action( 'manage_pages_custom_column', 'revealid_pages_id_column_content', 5, 2 );
+
+
+function revealid_add_pages_id_column( $columns ) {
+   $columns['revealid_id'] = 'ID';
+   return $columns;
+}
+
+function revealid_pages_id_column_content( $column, $id ) {
+  if( 'revealid_id' == $column ) {
+    echo $id;
+  }
+}
+
+
+
+
+
+/* Require Lib Folder & Files */
+require( get_stylesheet_directory() . '/lib/A.php' );
+
+
+
+
+
+/* Required plugins */
+require_once get_stylesheet_directory() . '/lib/bundled-plugins/required-plugins.php';
+
+
+
+
+
+/* Display Post IDs */
+add_filter( 'manage_posts_columns', 'revealid_add_post_id_column', 5 );
+add_action( 'manage_posts_custom_column', 'revealid_post_id_column_content', 5, 2 );
+
+
+function revealid_add_post_id_column( $columns ) {
+   $columns['revealid_id'] = 'ID';
+   return $columns;
+}
+
+function revealid_post_id_column_content( $column, $id ) {
+  if( 'revealid_id' == $column ) {
+    echo $id;
+  }
+}
+
+
+
+
+
+/* Add URL Column To WordPress Media Library */
+function muc_column( $cols ) {
+        $cols["media_url"] = "URL";
+        return $cols;
+}
+function muc_value( $column_name, $id ) {
+        if ( $column_name == "media_url" ) echo '<input type="text" width="100%" onclick="jQuery(this).select();" value="'. wp_get_attachment_url( $id ). '" readonly="true" />';
+}
+add_filter( 'manage_media_columns', 'muc_column' );
+add_action( 'manage_media_custom_column', 'muc_value', 10, 2 );
+
+
+
+
+
+/* Stop Adding Functions Below this Line */
