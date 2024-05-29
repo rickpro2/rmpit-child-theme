71087d550a703c0ff44a0db7ada1610ac8eff47f
Rickie M. Proctor II <rickie@rickieproctor.com>
2024-05-28 20:04:35 -0400

    updated required plugins

--- lib/bundled-plugins/required-plugins.php
+++ lib/bundled-plugins/required-plugins.php
@@ -10,7 +10,7 @@
  *
  * @package    TGM-Plugin-Activation
  * @subpackage Example
- * @version    2.6.1 for child theme OceanWp
+ * @version    2.6.1
  * @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
  * @copyright  Copyright (c) 2011, Thomas Griffin
  * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
@@ -31,9 +31,9 @@
  * Plugin:
  * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
  */
-require_once get_stylesheet_directory() . '/lib/bundled-plugins/class-tgm-plugin-activation.php';
+require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 
-add_action( 'tgmpa_register', 'rmpit_register_required_plugins' );
+add_action( 'tgmpa_register', 'my_theme_register_required_plugins' );
 
 /**
  * Register the required plugins for this theme.
@@ -52,14 +52,13 @@ add_action( 'tgmpa_register', 'rmpit_register_required_plugins' );
  *
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
-function rmpit_register_required_plugins() {
+function my_theme_register_required_plugins() {
 	/*
 	 * Array of plugin arrays. Required keys are name and slug.
 	 * If the source is NOT from the .org repo, then source is also required.
 	 */
 	$plugins = array(
 
-/*
 		// This is an example of how to include a plugin bundled with a theme.
 		array(
 			'name'               => 'TGM Example Plugin', // The plugin name.
@@ -72,180 +71,43 @@ function rmpit_register_required_plugins() {
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
-*/
 
+		// This is an example of how to include a plugin from an arbitrary external source in your theme.
 		array(
-			'name'      => 'Elementor Website Builder – More than Just a Page Builder',
-			'slug'      => 'elementor',
-		),
-		
-		array(
-			'name'      => 'Dashboard Welcome for Elementor',
-			'slug'      => 'dashboard-welcome-for-elementor',
+			'name'         => 'TGM New Media Plugin', // The plugin name.
+			'slug'         => 'tgm-new-media-plugin', // The plugin slug (typically the folder name).
+			'source'       => 'https://s3.amazonaws.com/tgm/tgm-new-media-plugin.zip', // The plugin source.
+			'required'     => true, // If false, the plugin is only 'recommended' instead of required.
+			'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader', // If set, overrides default API URL and points to an external URL.
 		),
 
+		// This is an example of how to include a plugin from a GitHub repository in your theme.
+		// This presumes that the plugin code is based in the root of the GitHub repository
+		// and not in a subdirectory ('/src') of the repository.
 		array(
-			'name'               => 'All-in-One WP Migration',
-			'slug'               => 'all-in-one-wp-migration',
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/all-in-one-wp-migration.zip',
-			'required'           => true,
-			'force_activation'   => true,
-		),
-		
-		array(
-			'name'               => 'All-in-One WP Migration Unlimited Extension',
-			'slug'               => 'all-in-one-wp-migration-unlimited-extension', 
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/all-in-one-wp-migration-unlimited-extension.zip', 
-			'required'           => true, 
-			'force_activation'   => true, 
+			'name'      => 'Adminbar Link Comments to Pending',
+			'slug'      => 'adminbar-link-comments-to-pending',
+			'source'    => 'https://github.com/jrfnl/WP-adminbar-comments-to-pending/archive/master.zip',
 		),
 
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
 		array(
-			'name'               => 'Solid Security Pro',
-			'slug'               => 'ithemes-security-pro',
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/ithemes-security-pro.zip',
+			'name'      => 'BuddyPress',
+			'slug'      => 'buddypress',
+			'required'  => false,
 		),
 
+		// This is an example of the use of 'is_callable' functionality. A user could - for instance -
+		// have WPSEO installed *or* WPSEO Premium. The slug would in that last case be different, i.e.
+		// 'wordpress-seo-premium'.
+		// By setting 'is_callable' to either a function from that plugin or a class method
+		// `array( 'class', 'method' )` similar to how you hook in to actions and filters, TGMPA can still
+		// recognize the plugin as being installed.
 		array(
-			'name'      => 'iThemes Security',
-			'slug'      => 'better-wp-security',
+			'name'        => 'WordPress SEO by Yoast',
+			'slug'        => 'wordpress-seo',
+			'is_callable' => 'wpseo_init',
 		),
-
-		array(
-			'name'      => 'User Role Editor',
-			'slug'      => 'user-role-editor',
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/user-role-editor.4.64.1.zip',
-			'required'  => true,
-			'force_activation'   => true,
-		),
-
-		array(
-			'name'               => 'Editor Switcher', // The plugin name.
-			'slug'               => 'editor-switcher', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/editor-switcher.zip',
-			'required'  => true,
-			'force_activation'   => true,
-		),
-		
-		array(
-			'name'      => 'Yoast Duplicate Post',
-			'slug'      => 'duplicate-post',
-			'required'  => true,
-			'force_activation'   => true,
-		),
-		
-		array(
-			'name'      => 'Yoast SEO',
-			'slug'      => 'wordpress-seo',
-		),
-
-		array(
-			'name'      => 'WooCommerce',
-			'slug'      => 'woocommerce',
-		),
-
-		array(
-			'name'      => 'File Manager',
-			'slug'      => 'wp-file-manager',
-		),
-		
-		array(
-			'name'      => 'Offload Media – Cloud Storage',
-			'slug'      => 'offload-media-cloud-storage',
-		),
-		
-		array(
-			'name'      => 'WooCommerce Square',
-			'slug'      => 'woocommerce-square',
-		),
-
-		array(
-			'name'      => 'WP Reset – Most Advanced WordPress Reset Tool',
-			'slug'      => 'wp-reset',
-		),
-
-		array(
-			'name'      => 'User Switching',
-			'slug'      => 'user-switching',
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/user-switching.1.7.0.zip',
-		),
-
-		array(
-			'name'      => 'Username Changer',
-			'slug'      => 'username-changer',
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/username-changer.3.2.2.zip',
-		),
-		
-		array(
-			'name'      => 'Simple Page Ordering',
-			'slug'      => 'simple-page-ordering',
-			'force_activation'   => true,
-		),
-		
-		array(
-			'name'      => 'Code Snippets',
-			'slug'      => 'code-snippets',
-		),
-
-		array(
-			'name'      => 'Really Simple SSL',
-			'slug'      => 'really-simple-ssl',
-		),
-		
-		array(
-			'name'      => 'Ocean Extra',
-			'slug'      => 'ocean-extra',
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/ocean-extra.2.2.1.zip',
-			'force_activation'   => true,
-			'required'  => true,
-		),
-		
-		array(
-			'name'      => 'Ocean Social Sharing',
-			'slug'      => 'ocean-social-sharing',
-		),
-				
-		array(
-			'name'      => 'Ocean Product Sharing',
-			'slug'      => 'ocean-product-sharing',
-		),
-				
-		array(
-			'name'      => 'Ocean Modal Window',
-			'slug'      => 'ocean-modal-window',
-		),
-	
-		array(
-			'name'      => 'Ocean Modal Window',
-			'slug'      => 'ocean-modal-window',
-		),
-		
-		array(
-			'name'      => 'Ocean Pro Demos',
-			'slug'      => 'ocean-pro-demos',
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/ocean-pro-demos.1.5.1.zip',
-			'force_activation'   => true,
-			'required'  => true,
-		),
-		
-		array(
-			'name'               => 'Ocean Elementor Widgets', // The plugin name.
-			'slug'               => 'ocean-elementor-widgets', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/ocean-elementor-widgets.2.4.4.zip', // The plugin source.
-		),
-	
-		array(
-			'name'               => 'Ocean Sticky Header', // The plugin name.
-			'slug'               => 'ocean-sticky-header', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/ocean-sticky-header.2.1.2.zip', // The plugin source.
-		),
-		
-		array(
-			'name'               => 'WooCrack Updater', // The plugin name.
-			'slug'               => 'woocrack', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/lib/bundled-plugins/plugins/woocrack-updater-plugin-2.0.zip', // The plugin source.
-		),
-
 
 	);
 
@@ -272,76 +134,76 @@ function rmpit_register_required_plugins() {
 
 		/*
 		'strings'      => array(
-			'page_title'                      => __( 'Install Required Plugins', 'rmpit' ),
-			'menu_title'                      => __( 'Install Plugins', 'rmpit' ),
+			'page_title'                      => __( 'Install Required Plugins', 'theme-slug' ),
+			'menu_title'                      => __( 'Install Plugins', 'theme-slug' ),
 			/* translators: %s: plugin name. * /
-			'installing'                      => __( 'Installing Plugin: %s', 'rmpit' ),
+			'installing'                      => __( 'Installing Plugin: %s', 'theme-slug' ),
 			/* translators: %s: plugin name. * /
-			'updating'                        => __( 'Updating Plugin: %s', 'rmpit' ),
-			'oops'                            => __( 'Something went wrong with the plugin API.', 'rmpit' ),
+			'updating'                        => __( 'Updating Plugin: %s', 'theme-slug' ),
+			'oops'                            => __( 'Something went wrong with the plugin API.', 'theme-slug' ),
 			'notice_can_install_required'     => _n_noop(
 				/* translators: 1: plugin name(s). * /
 				'This theme requires the following plugin: %1$s.',
 				'This theme requires the following plugins: %1$s.',
-				'rmpit'
+				'theme-slug'
 			),
 			'notice_can_install_recommended'  => _n_noop(
 				/* translators: 1: plugin name(s). * /
 				'This theme recommends the following plugin: %1$s.',
 				'This theme recommends the following plugins: %1$s.',
-				'rmpit'
+				'theme-slug'
 			),
 			'notice_ask_to_update'            => _n_noop(
 				/* translators: 1: plugin name(s). * /
 				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
 				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-				'rmpit'
+				'theme-slug'
 			),
 			'notice_ask_to_update_maybe'      => _n_noop(
 				/* translators: 1: plugin name(s). * /
 				'There is an update available for: %1$s.',
 				'There are updates available for the following plugins: %1$s.',
-				'rmpit'
+				'theme-slug'
 			),
 			'notice_can_activate_required'    => _n_noop(
 				/* translators: 1: plugin name(s). * /
 				'The following required plugin is currently inactive: %1$s.',
 				'The following required plugins are currently inactive: %1$s.',
-				'rmpit'
+				'theme-slug'
 			),
 			'notice_can_activate_recommended' => _n_noop(
 				/* translators: 1: plugin name(s). * /
 				'The following recommended plugin is currently inactive: %1$s.',
 				'The following recommended plugins are currently inactive: %1$s.',
-				'rmpit'
+				'theme-slug'
 			),
 			'install_link'                    => _n_noop(
 				'Begin installing plugin',
 				'Begin installing plugins',
-				'rmpit'
+				'theme-slug'
 			),
 			'update_link' 					  => _n_noop(
 				'Begin updating plugin',
 				'Begin updating plugins',
-				'rmpit'
+				'theme-slug'
 			),
 			'activate_link'                   => _n_noop(
 				'Begin activating plugin',
 				'Begin activating plugins',
-				'rmpit'
+				'theme-slug'
 			),
-			'return'                          => __( 'Return to Required Plugins Installer', 'rmpit' ),
-			'plugin_activated'                => __( 'Plugin activated successfully.', 'rmpit' ),
-			'activated_successfully'          => __( 'The following plugin was activated successfully:', 'rmpit' ),
+			'return'                          => __( 'Return to Required Plugins Installer', 'theme-slug' ),
+			'plugin_activated'                => __( 'Plugin activated successfully.', 'theme-slug' ),
+			'activated_successfully'          => __( 'The following plugin was activated successfully:', 'theme-slug' ),
 			/* translators: 1: plugin name. * /
-			'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'rmpit' ),
+			'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'theme-slug' ),
 			/* translators: 1: plugin name. * /
-			'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'rmpit' ),
+			'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'theme-slug' ),
 			/* translators: 1: dashboard link. * /
-			'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'rmpit' ),
-			'dismiss'                         => __( 'Dismiss this notice', 'rmpit' ),
-			'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'rmpit' ),
-			'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'rmpit' ),
+			'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'theme-slug' ),
+			'dismiss'                         => __( 'Dismiss this notice', 'theme-slug' ),
+			'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'theme-slug' ),
+			'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'theme-slug' ),
 
 			'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
 		),
