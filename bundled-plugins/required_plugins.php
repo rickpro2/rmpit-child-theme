1de7dde722dfe9fa2fe3c1390a872f36ef7364b2
Rickie M. Proctor II <rickie@rickieproctor.com>
2025-11-25 09:55:50 -0500

    3.1.1

--- bundled-plugins/required_plugins.php
+++ bundled-plugins/required_plugins.php
@@ -264,6 +264,7 @@ function rmpit_register_required_plugins() {
 			'name'               => 'User Switching',
 			'slug'               => 'user-switching',
 			'force_deactivation' => true,
+			'force_activation'   => true,			
 			'source'             => get_stylesheet_directory() . '/bundled-plugins/plugins/user-switching.1.7.0.zip',
 		),
 
@@ -271,12 +272,14 @@ function rmpit_register_required_plugins() {
 			'name'               => 'User Role Editor',
 			'slug'               => 'user-role-editor', 
 			'force_deactivation' => true,
+			'force_activation'   => true,
 			'source'             => get_stylesheet_directory() . '/bundled-plugins/plugins/user-role-editor.4.64.1.zip',
 		),
 
 		array(
 			'name'               => 'Username Changer',
 			'slug'               => 'username-changer', 
+			'force_activation'   => true,
 			'force_deactivation' => true,
 			'source'             => get_stylesheet_directory() . '/bundled-plugins/plugins/username-changer.3.2.2.zip',
 		),
